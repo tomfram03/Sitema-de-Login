f48b29b977353070eaf8e4dd1a40d5800f4971f3
tomfram03 <38266913+tomfram03@users.noreply.github.com>
2022-06-06 09:51:07 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -10,35 +10,34 @@ if (empty($dados['cadnome'])) {
     $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'> Erro: Necessário preeencher o campo e-mail!</div>"];
 } elseif (empty($dados['cadsenha'])) {
     $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'> Erro: Necessário preeencher o campo senha!</div>"];
-}else {
+} else {
 
     $querey_usuario_pes = "SELECT id FROM usuarios WHERE email=:email LIMIT 1";
     $result_usuario = $conn->prepare($querey_usuario_pes);
     $result_usuario->bindParam(':email', $dados['cademail'], PDO::PARAM_STR);
     $result_usuario->execute();
-    
-    if (($result_usuario) AND ($result_usuario->rowCount() != 0)){
+
+    if (($result_usuario) and ($result_usuario->rowCount() != 0)) {
         $retorna = ['erro' => true, 'msg' => "<div class='alert alert-alert' role='alert'>Erro: E-mail já está cadastrado!</div>"];
-    }else{
+    } else {
         $query_usuario = "INSERT INTO usuarios (nome, email, senha) VALUE(:nome, :email, :senha)";
-    $cad_usuario = $conn -> prepare($query_usuario);
-    $cad_usuario->bindParam(':nome', $dados['cadnome'], PDO::PARAM_STR);
-    $cad_usuario->bindParam(':email', $dados['cademail'], PDO::PARAM_STR);
-    $senha_cript = password_hash($dados['cadsenha'], PASSWORD_DEFAULT);
-    $cad_usuario->bindParam(':senha', $senha_cript, PDO::PARAM_STR);
-    
-    $cad_usuario->execute();
+        $cad_usuario = $conn->prepare($query_usuario);
+        $cad_usuario->bindParam(':nome', $dados['cadnome'], PDO::PARAM_STR);
+        $cad_usuario->bindParam(':email', $dados['cademail'], PDO::PARAM_STR);
+        $senha_cript = password_hash($dados['cadsenha'], PASSWORD_DEFAULT);
+        $cad_usuario->bindParam(':senha', $senha_cript, PDO::PARAM_STR);
 
-    if($cad_usuario->rowCount()){
-        $retorna = ['erro' => false, 'msg' => "<div class='alert alert-success' role='alert'> Usuário cadastrado com sucesso!</div>"];
-    }else{
-        $retorna = ['erro' => true, 'msg' => "<div class='alert alert-success' role='alert'>Erro: Usuário não cadastrado com sucesso!</div>"];
+        $cad_usuario->execute();
+
+        if ($cad_usuario->rowCount()) {
+
+            
+
+            $retorna = ['erro' => false, 'msg' => "<div class='alert alert-success' role='alert'> Usuário cadastrado com sucesso!</div>"];
+        } else {
+            $retorna = ['erro' => true, 'msg' => "<div class='alert alert-success' role='alert'>Erro: Usuário não cadastrado com sucesso!</div>"];
+        }
     }
-    }
-
-    
-
-    
 }
 
 echo json_encode($retorna);
